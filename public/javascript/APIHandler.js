6846ace1ccefdf5226cbdd1f819e8712e98d2908
Reshma Nidhish <reshmanidhish0@gmail.com>
2023-07-17 12:17:38 +0200

    iterations done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,49 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  async getFullList() {
+    try {
+      const response = await axios.get(`http://localhost:8000/characters`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error fetching characters');
+    }
   }
 
-  getOneRegister () {
-
+  async getOneRegister(id) {
+    try {
+      const response = await axios.get(`http://localhost:8000/characters/${id}`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error fetching character');
+    }
   }
 
-  createOneRegister () {
-
+  async createOneRegister(characterData) {
+    try {
+      const response = await axios.post(`http://localhost:8000/characters`, characterData);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error creating character');
+    }
   }
 
-  updateOneRegister () {
-
+  async updateOneRegister(id, characterData) {
+    try {
+      const response = await axios.patch(`http://localhost:8000/characters/${id}`, characterData);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error updating character');
+    }
   }
 
-  deleteOneRegister () {
-
+  async deleteOneRegister(id) {
+    try {
+      await axios.delete(`http://localhost:8000/characters/${id}`);
+    } catch (error) {
+      throw new Error('Error deleting character');
+    }
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,88 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI
+    .getFullList()
+    .then((characters) => {
+      const charactersContainer = document.getElementById("characters-container");
+      charactersContainer.innerHTML = "";
+      for (const character of characters) {
+        const characterInfo = document.createElement("div");
+        characterInfo.classList.add("character-info");
+        characterInfo.innerHTML = `
+          <div class="name"> Character ID: ${character.id} </div> 
+          <div class="name"> Character Name: ${character.name} </div> 
+          <div class="occupation"> Character Occupation: ${character.occupation}</div>
+          <div class="cartoon">Is a Cartoon? ${character.cartoon}</div>
+          <div class="weapon">Character Weapon: ${character.weapon}</div>
+        `;
+        charactersContainer.appendChild(characterInfo);
+      }
+    })
+    .error(console.log(error));
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    const characterId = document.getElementById("character-id").value;
+    console.log(characterId);
+    charactersAPI
+      .getOneRegister(characterId)
+      .then((character) =>
+        {
+          const charactersContainer = document.getElementById("characters-container");
+          charactersContainer.innerHTML = "";
+          const characterInfo = document.createElement("div");
+          characterInfo.classList.add("character-info");
+          characterInfo.innerHTML = `
+            <div class="name"> Character ID: ${character.id} </div> 
+            <div class="name"> Character Name: ${character.name} </div> 
+            <div class="occupation"> Character Occupation: ${character.occupation}</div>
+            <div class="cartoon">Is a Cartoon? ${character.cartoon}</div>
+            <div class="weapon">Character Weapon: ${character.weapon}</div>
+          `;
+          charactersContainer.appendChild(characterInfo);
+        }
+      )
+      .catch((error) => {
+        document.getElementById("characters-container").innerText = "";
+          document.getElementById(
+            "characters-container"
+          ).innerHTML = "Not Found"
+      });
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    const characterId = document.getElementById("character-id-delete").value;
+    charactersAPI
+      .deleteOneRegister(characterId)
+      .then()
+      .catch((error) => console.log(error));
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    const characterId = document.getElementById("chr-id").value;
+    const characterInfo = {
+      name: document.getElementById("name").value,
+      occupation: document.getElementById("occupation").value,
+      cartoon: document.getElementById("cartoon").value,
+      weapon: document.getElementById("weapon").value,
+    };
+    charactersAPI
+      .updateOneRegister(characterId, characterInfo)
+      .then()
+      .catch((error) => console.log(error));
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    const characterInfo = {
+      name: document.getElementById("name").value,
+      occupation: document.getElementById("occupation").value,
+      cartoon: document.getElementById("cartoon").value,
+      weapon: document.getElementById("weapon").value,
+    };
+    charactersAPI
+      .createOneRegister(characterInfo)
+      .then()
+      .catch((error) => console.log(error));
   });
 });
